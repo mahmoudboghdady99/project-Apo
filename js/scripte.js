590a469f6dfd73456dd3f40018271162660d3629
mahmoudboghdady99 <mahmoud995boghdady@gmail.com>
2023-02-10 15:24:33 +0200

    ADD new features

--- js/scripte.js
+++ js/scripte.js
@@ -1,12 +1,13 @@
-function getPosts(){
+function getPosts(userId){
     let request = new XMLHttpRequest()
-    request.open("GET", "https://jsonplaceholder.typicode.com/posts")
+    request.open("GET", "https://jsonplaceholder.typicode.com/posts?userId=" +userId)
     request.responseType = "json"
     request.send()
     request.onload = function(){
         if(request.status>=200 && request.status < 300){
           let posts = request.response
-          document.querySelector(".subject-content").innerHTML =""
+          let subject = document.querySelector(".subject-content")
+          subject.innerHTML =""
           for(post of posts){
             let content= `  
             <div class="subject-content">
@@ -15,7 +16,8 @@ function getPosts(){
         </div>
 
             `
-        document.querySelector(".subject-content").innerHTML += content
+            subject.innerHTML +=content
+    
           }
 
         }else{
@@ -23,4 +25,42 @@ function getPosts(){
         }
     }
 }
-getPosts()
+function getUsers(){
+    let request = new XMLHttpRequest()
+    request.open("GET", "https://jsonplaceholder.typicode.com/users")
+    request.responseType = "json"
+    request.send()
+    request.onload = function(){
+        if(request.status>=200 && request.status < 300){
+          let users = request.response
+          let user_info = document.querySelector(".user")
+          user_info.innerHTML =""
+          for(user of users){
+            let content= `  
+            <div class="user-info" onclick="userClicked(${user.id}, this)" >
+            <h3>${user.name}</h3>
+            <h3>${user.email} </h3>
+        </div>
+
+            `
+            user_info.innerHTML +=content
+    
+          }
+
+        }else{
+            alert("Error")
+        }
+    }
+}
+getPosts(1)
+getUsers()
+
+function userClicked(id, el){
+  getPosts(id)
+
+  let sellectEl=document.getElementsByClassName("sellect")
+  for(element of sellectEl){
+    element.classList.remove("sellect")
+  }
+  el.classList.add("sellect")
+}
